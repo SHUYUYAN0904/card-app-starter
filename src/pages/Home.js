a64160d383ff86c3007b2b7b4719ba5a99297936
browniecakese <23031962@myrp.edu.sg>
2026-01-22 13:08:44 +0800

    commenting out styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,19 +3,31 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <main style={styles.container}>
-      <h1 style={styles.title}>Card App</h1>
+    <main 
+    // style={styles.container}
+    >
+      <h1 
+      // style={styles.title}
+      >Card App</h1>
 
-      <p style={styles.subtitle}>
+      <p 
+      // style={styles.subtitle}
+      >
         Welcome to the Card App.  
       </p>
 
-      <div style={styles.actions}>
-        <Link to="/cards" style={styles.button}>
+      <div 
+      // style={styles.actions}
+      >
+        <Link to="/cards" 
+        // style={styles.button}
+        >
           View Cards
         </Link>
 
-        <Link to="/add" style={styles.buttonAlt}>
+        <Link to="/cards/new" 
+        // style={styles.buttonAlt}
+        >
           Add Card
         </Link>
       </div>
